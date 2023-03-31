fe3e7df898eb91b535d5b9631667ad1616a1b08d
nate-rogers <77183225+nate-rogers@users.noreply.github.com>
2023-03-30 22:08:33 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<h1>
+    Identify, Protect, Detect, Respond, Recover
+</h1>
+
+<body>
+    <h2>
+        No matter what happens, SecureYou is here for you
+    </h2>
+    <p>
+        We are a young up and coming business from the Fort Collins community who cares about your digital safety
+    </p>
+    <!--Image here-->
+</body>
