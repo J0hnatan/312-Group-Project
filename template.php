4398bb4885712e4f0a48c2f741f80d14fd451a25
J0hnatan <98071355+J0hnatan@users.noreply.github.com>
2023-03-30 18:30:43 -0600

    Create template.php

---
+++ template.php
@@ -0,0 +1,28 @@
+<head>
+    <title>
+        <?php echo $title ?>
+    </title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Jonathan Ibarra">
+    <meta name="description" content="CS 312: FuelPHP Project">
+    <?php echo Asset::css($css) ?>
+</head>
+
+
+
+<body>
+    <img>
+    <h1><!--fictitious company name--></h1>
+    <nav>
+        <ul>
+            <li><a href="https://cs.colostate.edu:4444/~jibarra1/m1/index/m1/index">Home</a></li>
+            <li><a href="https://cs.colostate.edu:4444/~jibarra1/m1/index/m1/about">About</a></li>
+            <li><a href="https://cs.colostate.edu:4444/~jibarra1/m1/index/m1/color">Color Coordinate</a></li>
+            
+        </ul>
+    </nav>
+    <?php echo $content ?>
+    <footer>
+        <p>For the CSU class CS 312</p>
+    </footer>
+</body>
