868924f862a02af62d62e54ab85e392a57978ca9
chardol <90341682+chardol@users.noreply.github.com>
2023-03-31 19:29:51 -0600

    Update about.php

--- about.php
+++ about.php
@@ -5,26 +5,28 @@
 <br>
 
 <main>
+    <div class="bios">
     <h3>
         Nate
     </h3>
-    <!--Image here-->
+    <?php echo Asset::img('nate.jpeg', array('class' => 'image', 'width' => '200px')); ?>
     <p>
         Nate likes protecting our clients digital footprint and the movie "The Sixth Sense" featuring the great Bruce Willis. 
     </p>
     <h3>
         Charli
     </h3>
-    <!--Image here-->
+    <?php echo Asset::img('charli.jpg', array('class' => 'image', 'width' => '200px')); ?>
     <p>
         Charli enjoys long walks on the beach and smooth jazz. When Charli isn't saving the world on her computer, she's prolly climbing Mt. Everest.
     </p>
     <h3>
         Jonathan
     </h3>
-    <!--Image here-->
+    <?php echo Asset::img('jonathan.jpg', array('class' => 'image', 'width' => '200px')); ?>
     <p>
         Jonathan is 6'7" and benches 4 plates, he's as good at security on the computer as off of it.
     </p>
+    </div>
 </main>
 
