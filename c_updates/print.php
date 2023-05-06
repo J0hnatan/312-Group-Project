7493edb5bec35b0461c5f9ca67e2031e56a00b61
chardol <90341682+chardol@users.noreply.github.com>
2023-05-05 23:11:28 -0600

    Create print.php

---
+++ c_updates/print.php
@@ -0,0 +1,61 @@
+<table>
+    <?php
+        for($i=0; $i< $colors; $i++){
+                if($i==0) {
+                        echo "<tr><td>$color0</td><td></td></tr>";
+                }
+                if($i==1) {
+                        echo "<tr><td>$color1</td><td></td></tr>";
+                }
+                if($i==2) {
+                        echo "<tr><td>$color2</td><td></td></tr>";
+                }
+                if($i==3) {
+                        echo "<tr><td>$color3</td><td></td></tr>";
+                }
+                if($i==4) {
+                        echo "<tr><td>$color4</td><td></td></tr>";
+                }
+                if($i==5) {
+                        echo "<tr><td>$color5</td><td></td></tr>";
+                }
+                if($i==6) {
+                        echo "<tr><td>$color6</td><td></td></tr>";
+                }
+                if($i==7) {
+                        echo "<tr><td>$color7</td><td></td></tr>";
+                }
+                if($i==8) {
+                        echo "<tr><td>$color8</td><td></td></tr>";
+                }
+                if($i==9) {
+                        echo "<tr><td>$color9</td><td></td></tr>";
+                }
+        }
+    ?>
+</table>
+
+<table>
+    <?php
+    $letters = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+  
+        /*echo "<tr><td></td>";
+        for($i=0; $i<$rows_cols; $i++){
+            echo "<td>$letters[$i]</td>";
+        }
+        echo "</tr>";
+
+        
+        for($i =1; $i <$rows_cols; $i++){
+            echo "<tr>";
+            for($j=0; $j < $rows_cols; $j++){
+                if($j >0){
+                    echo "<td></td>";
+                }else{
+                    echo "<td>$j</td>";
+                }
+            }
+            echo "</tr>";
+        }*/
+    ?>
+</table> 
