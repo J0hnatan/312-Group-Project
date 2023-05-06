9b30faf98f9414dfd1efdccfa09234fbdc686904
chardol <90341682+chardol@users.noreply.github.com>
2023-05-05 23:15:49 -0600

    Update m1.php

--- c_updates/m1.php
+++ c_updates/m1.php
@@ -32,7 +32,49 @@ class Controller_M1 extends Controller_Template
         }
         public function action_print()
         {
+                $count = 0;
                 $data['title'] = "Print View";
+                if(isset($_GET['color0'])) {
+                        $data['color0'] = $_GET['color0'];
+                        $count++;
+                }
+                if(isset($_GET['color1'])) {
+                        $data['color1'] = $_GET['color1'];
+                        $count++;
+                }
+                if(isset($_GET['color2'])) {
+                        $data['color2'] = $_GET['color2'];
+                        $count++;
+                }
+                if(isset($_GET['color3'])) {
+                        $data['color3'] = $_GET['color3'];
+                        $count++;
+                }
+                if(isset($_GET['color4'])) {
+                        $data['color4'] = $_GET['color4'];
+                        $count++;
+                }
+                if(isset($_GET['color5'])) {
+                        $data['color5'] = $_GET['color5'];
+                        $count++;
+                }
+                if(isset($_GET['color6'])) {
+                        $data['color6'] = $_GET['color6'];
+                        $count++;
+                }
+                if(isset($_GET['color7'])) {
+                        $data['color7'] = $_GET['color7'];
+                        $count++;
+                }
+                if(isset($_GET['color8'])) {
+                        $data['color8'] = $_GET['color8'];
+                        $count++;
+                }
+                if(isset($_GET['color9'])) {
+                        $data['color9'] = $_GET['color9'];
+                        $count++;
+                }
+                $data['colors'] = $count;
                 return new Response(View::forge('m1/print.php', $data));
         }
 }
